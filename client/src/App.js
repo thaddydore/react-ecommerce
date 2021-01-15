8c6d49ebc06f940d0645de43813373b782b561ba
thaddydore <thaddydore@gmail.com>
2021-01-15 08:47:58 +0100

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>welcome to my personal shop</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
