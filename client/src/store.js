e6130a83e54eb2a663dc676f56f73bd5a00dc6dc
thaddydore <thaddydore@gmail.com>
2021-01-23 14:13:13 +0100

    wip: client login screen

--- client/src/store.js
+++ client/src/store.js
@@ -4,18 +4,24 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { productList, productDetails } from './reducers/product.js';
 import { cart } from './reducers/cart.js';
+import { userLogin } from './reducers/user'
 
 const reducer = combineReducers({
   productList,
   productDetails,
   cart,
+  userLogin,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ?
   JSON.parse(localStorage.getItem('cartItems')) : [];
 
+const userInfoFromStorage = localStorage.getItem('userInfo') ?
+  JSON.parse(localStorage.getItem('userInfo')) : null;
+
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage }
 };
 
 const middleware = [thunk];
