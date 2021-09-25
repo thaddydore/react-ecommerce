6852a0d619b4fc142acb48911206446e2896a563
thaddydore <thaddydore@gmail.com>
2021-09-25 23:54:34 +0100

    enable cors

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import axios from 'axios';
 // import logo from './logo.svg';
 // import './App.css';
 import store from './store.js';
